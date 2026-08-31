6b9f7d3cf927fb73309bbb59a036c0d8bebfd5e7
marcin_szlaz <marcinszlaz@gmail.com>
2026-08-31 21:18:42 +0200

    struggle with chapter six ;-)

--- models.py
+++ models.py
@@ -26,7 +26,7 @@ class Product(Model):
         ForeignKey('manufacturers.id'), index = True) #or Manufacturer.id
     year: Mapped[int] = mapped_column(index = True)
     cpu: Mapped[Optional[str]] = mapped_column(String(32)) # optional[str] = can be NULL
-# objects
+# relationship objects
     manufacturer: Mapped['Manufacturer'] = relationship(
         back_populates = 'products') # lazy = 'joined' => eager loader active
     countries: Mapped[list['Country']] = relationship(
@@ -45,52 +45,56 @@ class Product(Model):
 # one manufacturer to many products relationship
 class Manufacturer(Model):
     __tablename__ = 'manufacturers'
-
+# columns
     id: Mapped[int] = mapped_column(primary_key = True)
     name: Mapped[str] = mapped_column(String(64), index = True, unique = True)
+# relationship objects
     products: Mapped[list['Product']] = relationship(
-        cascade = 'all, delete-orphan', back_populates = 'manufacturer') # cascade 'save-update, merge' -> default setting, 'all, delete-orphan'
+        cascade = 'all, delete-orphan', back_populates = 'manufacturer') # cascade 
+    # 'save-update, merge' -> default setting, 'all, delete-orphan'
 
     def __repr__(self):
         return f'Manufacturer({self.id}, "{self.name}")'
 
-
 class Country(Model):
     __tablename__ = 'countries'
-
+# columns
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(32), index=True, unique=True)
-    
-    products: Mapped[list['Product']] = relationship(secondary='products_countries', back_populates='countries')
+# relationship objects
+    products: Mapped[list['Product']] = relationship(secondary='products_countries', 
+    back_populates='countries')
+
     def __repr__(self):
         return f'Country({self.id}, "{self.name}")'
 
 class Customer(Model):
     __tablename__ = 'customers'
+# columns
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
     address: Mapped[Optional[str]] = mapped_column(String(128))
     phone: Mapped[Optional[str]] = mapped_column(String(32))
-    
+# relationship objects
     orders: WriteOnlyMapped['Order'] = relationship(
         back_populates='customer')
     product_reviews: WriteOnlyMapped['ProductReview'] = relationship(
         back_populates='customer')
     blog_users: WriteOnlyMapped['BlogUser'] = relationship(
         back_populates='customer')
-    
+
     def __repr__(self):
         return f"Customer({self.id.hex}, {self.name})"
 
 class Order(Model):
     __tablename__ = 'orders'
-
+# columns
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     timestamp: Mapped[datetime] = mapped_column(
     default=lambda:datetime.now(UTC), index=True)
     customer_id: Mapped[UUID] = mapped_column(ForeignKey('customers.id'),
      index=True)
-    # objects
+# relationship objects
     customer: Mapped['Customer'] = relationship(back_populates='orders')
     order_items: Mapped[list['OrderItem']] = relationship(
     back_populates='order')
@@ -98,65 +102,75 @@ class Order(Model):
     def __repr__(self):
         return f"Order({self.timestamp}, {self.id.hex})"
 
-# In case when you join table has their own columns, besides foreign
+# In case when your join table has their own columns, besides foreign
 # keys as primary key, you have to use full class inherits from Model class.
-# Another implications of table has additional columns is tha, we can't
+# Another implications of table has additional columns is that, we can't
 # use `secondary` parameter in relationship function.
 class OrderItem(Model):
     __tablename__ = 'orders_items'
-
+# columns
     product_id: Mapped[int] = mapped_column(ForeignKey('products.id'),
     primary_key=True)
     order_id: Mapped[UUID] = mapped_column(ForeignKey('orders.id'),
     primary_key=True)
     unit_price: Mapped[float]
     quantity: Mapped[int]
-    
+# relationship objects
     product: Mapped['Product'] = relationship(back_populates='order_items')
     order: Mapped['Order'] = relationship(back_populates='order_items')
 
-
 class ProductReview(Model):
     __tablename__ = 'products_reviews'
-
+# columns
     product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), 
     primary_key=True)
     customer_id: Mapped[UUID] = mapped_column(ForeignKey('customers.id'),
     primary_key=True)
-
     timestamp: Mapped[datetime] = mapped_column(
     default=lambda:datetime.now(UTC), index=True)
     rating: Mapped[int]
     comment: Mapped[Optional[str]] = mapped_column(Text)
-# objects
+# relationship objects
     product: Mapped['Product'] = relationship(back_populates='reviews')
     customer: Mapped['Customer'] = relationship(
         back_populates='product_reviews')
 
 class BlogArticle(Model):
     __tablename__ = 'blog_articles'
-
+# columns
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(128), index=True)
     author_id: Mapped[int] = mapped_column(ForeignKey('blog_authors.id'),
                                            index=True)
     product_id: Mapped[Optional[int]] = mapped_column(
-        ForeignKey('product.id'), index=True)
+        ForeignKey('products.id'), index=True)
+    language_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey('languages.id'), index=True)
+    translation_of_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey('blog_articles.id'), index=True)
     timestamp: Mapped[datetime] = mapped_column(
         default=lambda: datetime.now(UTC), index=True)
+# relationship objects
     author: Mapped['BlogAuthor'] = relationship(back_populates='articles')
     product: Mapped[Optional['Product']] = relationship(
         back_populates='blog_articles')
+    views: WriteOnlyMapped['BlogView'] = relationship(back_populates='article')
+    language: Mapped[Optional['Language']] = relationship(
+        back_populates='blog_articles')
+    translation_of: Mapped[Optional['BlogArticle']] = relationship(
+        remote_side=id, back_populates='translations')
+    translations: Mapped[list['BlogArticle']] = relationship(
+        back_populates='translation_of')
 
     def __repr__(self):
         return  f'BlogArticle({self.id}, "{self.title}")'
 
 class BlogAuthor(Model):
     __tablename__ = 'blog_authors'
-
+# columns
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), index=True)
-
+# relationship object
     articles: WriteOnlyMapped['BlogArticle'] = relationship(
         back_populates='author')
 
@@ -165,11 +179,11 @@ class BlogAuthor(Model):
 
 class BlogUser(Model):
     __tablename__ = 'blog_users'
-
+# columns
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     customer_id: Mapped[Optional[UUID]] = mapped_column(
         ForeignKey('customers.id'), index=True)
-
+# relationship objects
     customer: Mapped[Optional['Customer']] = relationship(
         back_populates='blog_users')
     sessions: WriteOnlyMapped['BlogSession'] = relationship(
@@ -180,41 +194,58 @@ class BlogUser(Model):
 
 class BlogSession(Model):
     __tablename__ = 'blog_sessions'
-
+# columns
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     user_id: Mapped[UUID] = mapped_column(ForeignKey('blog_users.id'),
                                           index=True)
+# relationship objects
     user: Mapped['BlogUser'] = relationship(back_populates='sessions')
+    views: WriteOnlyMapped['BlogView'] = relationship(back_populates = 'session')
 
     def __repr__(self):
         return f'BlogSession({self.id.hex})'
 
+class BlogView(Model):
+   __tablename__ = 'blog_views'
+# columns
+   id: Mapped[int] = mapped_column(primary_key = True)
+   article_id: Mapped[int] = mapped_column(ForeignKey('blog_articles.id'))
+   session_id: Mapped[UUID] = mapped_column(ForeignKey('blog_sessions.id'))
+   timestamp: Mapped[datetime] = mapped_column(default=datetime.now(UTC),
+                                               index=True)
+# relationship objects
+   article: Mapped['BlogArticle'] = relationship(back_populates='views')
+   session: Mapped['BlogSession'] = relationship(back_populates='views')
+
+class Language(Model):
+    __tablename__ = 'languages'
+# columns
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(32), index=True, unique=True)
+# relationship object
+    blog_articles: WriteOnlyMapped['BlogArticle'] = relationship(
+        back_populates='language')
+
+    def __repr__(self):
+        return f'Language({self.id}, "{self.name}")'
 
 
+"""
+c64 = Product(name = 'Commodore 64', manufacturer = 'Commodore')
+longer version
+with Session(engine) as session:
+    try:
+        session.add(c64)
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    print(c64)
 
-
-
-
-
-
-
-
-
-
-#c64 = Product(name = 'Commodore 64', manufacturer = 'Commodore')
-#longer version
-#with Session(engine) as session:
-#    try:
-#        session.add(c64)
-#        session.commit()
-#    except:
-#        session.rollback()
-#        raise
-#    print(c64)
-
-#shorter version with begin()
-#begin() under the hood do try/except magic
-#with Session(engine) as session:
-#    with session.begin():
-#        session.add(c64)
-#    print(c64)
+shorter version with begin()
+begin() under the hood do try/except magic
+with Session(engine) as session:
+    with session.begin():
+        session.add(c64)
+    print(c64)
+"""
